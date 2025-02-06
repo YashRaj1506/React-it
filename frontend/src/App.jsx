d4f6ccc0e28a4ab84862b57e41e01ffbdd0e87ce
YashRaj1506 <yashraj504300@gmail.com>
2025-02-07 00:36:48 +0530

    exporting componenets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,26 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MovieCard from './components/MovieCard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <p>Hello World</p>
-      </div>
-      <Text />
-    </>
-  )
-}
+    <MovieCard movie={{title: "Tim's Film", release_date: "2024"}}/>
+    <MovieCard movie={{title: "Joe's Film", release_date: "2024"}}/>
 
-// dummy component
-function Text() {
-  return (
-    <div>
-      <p>Hello World</p>
-    </div>
+    </>
   )
 }
 
