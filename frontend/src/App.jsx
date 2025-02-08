09077340da17e81255b96c6a80bfa59dc3cfd2d4
YashRaj1506 <yashraj504300@gmail.com>
2025-02-08 14:49:53 +0530

    url routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import MovieCard from './components/MovieCard'
 import Home from "./pages/Home"
+import Favourites from './pages/Favorties'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <> 
-    <Home />
-    </>
+    <div>
+      <NavBar />
+
+    <main className='main-content'>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/Favorites" element={<Favourites />}/>
+      </Routes>
+    </main>
+    </div>
   );
 }
 
